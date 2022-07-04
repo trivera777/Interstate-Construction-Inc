111d7ae2155783ca3f1e88440f6a70a6b37cf032
Tifanny Rivera <trivera51580@gmail.com>
2022-07-03 18:43:47 -0600

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import logo from '../src/interstate.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-          Learn React
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        logo
       </header>
     </div>
   );
