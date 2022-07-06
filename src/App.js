6de43aeecb29af9e96120ca210b1aba1c722130d
Tifanny Rivera <trivera51580@gmail.com>
2022-07-06 12:13:51 -0600

    added projects and contact component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+  <Navbar />
+  <About />
+  <Projects />
+  <Contact />
+  </>
+  )
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function About(){
+    return (
+        <div className="home">about</div>
+    )
+}
+
+export default About;
