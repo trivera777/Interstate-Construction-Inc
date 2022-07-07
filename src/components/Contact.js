e3863dac6366067b2210a5103e7922fd9fce1195
Tifanny Rivera <trivera51580@gmail.com>
2022-07-06 21:10:24 -0600

    added submit button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 function Contact() {
   return (
-    <Form>
-      <Form.Group className="mb-3" id="contact">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Your Message</Form.Label>
-        <Form.Control as="textarea" placeholder="Message..." rows={3} />
-      </Form.Group>
-    </Form>
+    <div>
+      <Form>
+        <Form.Group className="mb-3" id="contact">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Your Message</Form.Label>
+          <Form.Control as="textarea" placeholder="Message..." rows={3} />
+        </Form.Group>
+        <Button variant="light">Submit</Button>
+      </Form>
+    </div>
   );
 }
 
