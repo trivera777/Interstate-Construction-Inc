7ba27edcee0b86513f9497577e0feac7a9486589
Tifanny Rivera <trivera51580@gmail.com>
2022-07-06 21:18:52 -0600

    added name text field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card } from "react-bootstrap";
 
 function Contact() {
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3" id="contact">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Your Message</Form.Label>
-          <Form.Control as="textarea" placeholder="Message..." rows={3} />
-        </Form.Group>
-        <Button variant="light">Submit</Button>
-      </Form>
+      <Card style={{ width: "50rem" }}>
+        <Card.Body>
+          <Card.Title>Contact Us</Card.Title>
+          <br />
+          <Card.Text>
+            <Form>
+              <Form.Group className="mb-4" id="contact">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="name" placeholder="First Last" />
+              </Form.Group>
+              <Form.Group className="mb-4" id="contact">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Your Message</Form.Label>
+                <Form.Control as="textarea" placeholder="Message..." rows={3} />
+              </Form.Group>
+              <Button variant="primary">Submit</Button>
+            </Form>
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
