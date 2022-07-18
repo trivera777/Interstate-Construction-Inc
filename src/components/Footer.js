775d651ce211179df5a65ec81f8fe6fe281cdf40
Tifanny Rivera <trivera51580@gmail.com>
2022-07-17 19:33:14 -0600

    added footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
 import React from "react";
 import './footer.css'
-import { Nav } from "react-bootstrap";
+// import { Nav } from "react-bootstrap";
 
 
 function Footer() {
   return (
-  <footer>
-      ©footer
-    <div className="links">
-    <Nav.Link href="#about">Home</Nav.Link>
-            <Nav.Link eventKey={2} href="#projects">
-              Our Properties
-            </Nav.Link>
-            <Nav.Link eventKey={3} href="#contact">
-              Contact Us
-            </Nav.Link>
-    </div>
+  <footer className="footer-basic">
+    <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Company Name © 2018</p>
   </footer>
   );
 }
