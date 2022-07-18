e236a7004e2b62fb461c60cf6910f3e1d5ebaee8
Tifanny Rivera <trivera51580@gmail.com>
2022-07-17 19:37:18 -0600

    added correct hrefs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,9 @@ function Footer() {
   return (
   <footer className="footer-basic">
     <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#about">Home</a></li>
+                <li class="list-inline-item"><a href="#projects">Our Properties</a></li>
+                <li class="list-inline-item"><a href="#contact">Contact Us</a></li>
                 <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
             </ul>
             <p class="copyright">Company Name © 2018</p>
